2117b93ff991d5c004d2f3c791958e80abea17eb
tobias <tobias.leong@zendesk.com>
2022-12-17 01:41:39 +0800

    Update snow function

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
-let systems;
 let width;
 let audio;
 let img;
 let frame = 0;
 let height = 600;
+let snowflakes = [];
 
 function preload() {
     img = loadImage('static/hands.png');
@@ -21,108 +21,80 @@ function setup() {
 function draw() {
     background(51);
     background(0);
-    image(img, width/3 - 50, 0);
-    for (i = 0; i < systems.length; i++) {
-        systems[i].run();
-        systems[i].addParticle();
-    }
-    if (systems.length == 0) {
-        // default loading screen
-        fill(255);
+    imageMode(CENTER);
+    image(img, width / 2, height / 2 - 100);
+    let t = frameCount / 60; // update time
+
+    if (snowflakes.length > 0) {
+        snowflakes.push(new snowflake());
+    } else {
         textAlign(CENTER);
         textSize(32);
-        text("初恋", width / 2, height / 2 + 200);
+        text("初恋", width / 2, height / 4);
     }
-    frame += 1;
-}
-
-function getRandomPos() {
-    const x = Math.floor(Math.random() * (width - 10 + 1) + 10);
-    const y = Math.floor(Math.random() * (height - 10 + 1) + 10);
-    return { x, y }
-}
-
-function mousePressed() {
-    this.p = new ParticleSystem(createVector(mouseX, mouseY));
-    systems.push(p);
-    if (audio.paused) {
-        audio.play();
+    
+    // loop through snowflakes with a for..of loop
+    for (let flake of snowflakes) {
+        flake.update(t); // update snowflake position
+        flake.display(); // draw snowflake
     }
 }
+
+function snowflake() {
+    // initialize coordinates
+    this.posX = 0;
+    this.posY = random(600, 0);
+    this.initialangle = random(0, 2 * PI);
+    this.size = random(2, 5);
+
+    // radius of snowflake spiral
+    // chosen so the snowflakes are uniformly spread out in area
+    this.radius = sqrt(random(pow(width / 2, 2)));
+
+    this.update = function (time) {
+        // x position follows a circle
+        let w = 0.3; // angular speed
+        let angle = w * time + this.initialangle;
+        this.posX = width / 2 + this.radius * sin(angle);
+
+        // different size snowflakes fall at slightly different y speeds
+        this.posY += pow(this.size, 0.5);
+
+        // delete snowflake if past end of screen
+        if (this.posY > height) {
+            let index = snowflakes.indexOf(this);
+            snowflakes.splice(index, 1);
+        }
+    };
+
+    this.display = function () {
+        ellipse(this.posX, this.posY, this.size);
+    };
+}
+
+// function mousePressed() {
+//     this.p = new ParticleSystem(createVector(mouseX, mouseY));
+//     systems.push(p);
+//     if (audio.paused) {
+//         audio.play();
+//     }
+// }
 
 function keyPressed() {
     if (keyCode === LEFT_ARROW) {
-        console.log(systems.pop());
+        for (let i = 0; i < random(50); i++) {
+            snowflakes.pop();
+            console.log(snowflakes.length);
+        }
     } else if (keyCode === RIGHT_ARROW) {
-        const { x, y } = getRandomPos();
-        this.p = new ParticleSystem(createVector(x, y));
-        systems.push(p);
-        console.log(`Added particle at {${x}, ${y}}`);
+        for (let i = 0; i < random(250); i++) {
+            snowflakes.push(new snowflake()); 
+        }
     } else if (keyCode === 32) {
         if (audio.paused) {
             audio.play();
         } else {
-            audio.pause(); 
+            audio.pause();
         }
     }
 }
-
-let Particle = function (position) {
-    this.acceleration = createVector(0, 0.05);
-    this.velocity = createVector(random(-1, 1), random(-1, 0));
-    this.position = position.copy();
-    this.lifespan = 220;
-};
-
-Particle.prototype.run = function () {
-    this.update();
-    this.display();
-};
-
-Particle.prototype.update = function () {
-    this.velocity.add(this.acceleration);
-    this.position.add(this.velocity);
-    this.lifespan -= 2;
-};
-
-Particle.prototype.display = function () {
-    stroke(200, this.lifespan);
-    strokeWeight(2);
-    fill(127, this.lifespan);
-    ellipse(this.position.x, this.position.y, 12, 12);
-};
-
-Particle.prototype.isDead = function () {
-    if (this.lifespan < 0) {
-        return true;
-    } else {
-        return false;
-    }
-};
-
-let ParticleSystem = function (position) {
-    this.lifespan = 1000;
-    this.origin = position.copy();
-    this.particles = [];
-};
-
-ParticleSystem.prototype.isDead = function () {
-    return this.lifespan < 0;
-}
-
-ParticleSystem.prototype.addParticle = function () {
-    p = new Particle(this.origin);
-    this.particles.push(p);
-};
-
-ParticleSystem.prototype.run = function () {
-    this.lifespan -= 2;
-    for (let i = this.particles.length - 1; i >= 0; i--) {
-        let p = this.particles[i];
-        p.run();
-        if (p.isDead()) {
-            this.particles.splice(i, 1);
-        }
-    }
-
-};
